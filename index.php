d5955ed64715aa85443d7a10df84c24722dbfd57
uher11 <uherprod@gmail.com>
2025-01-14 23:20:39 +0700

    Menambahkan list pada file index.php

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia error minus fugiat repellendus corrupti distinctio dolor rerum perspiciatis nobis voluptas eveniet ducimus, molestiae expedita ipsa. Eligendi ratione eum dignissimos consequuntur.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
